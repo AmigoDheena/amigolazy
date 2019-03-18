f5f55425ed891ff0bff52c55d2353e48559a5158
Amigo Dheena <AmigoDheena@users.noreply.github.com>
2019-03-18 18:04:57 +0530

    removed console log

--- amigolazy.js
+++ amigolazy.js
@@ -3,8 +3,6 @@ for(let i=0; i<amframe.length;i++){
     let amsrc = amframe[i];
     let amdata = amsrc.getAttribute("data-src");
     let datanew = amsrc.getAttribute("lazy");
-    console.log(amdata);
-    console.log(datanew);
     setTimeout(function(){
     amframe[i].setAttribute("src",amdata);
 }, datanew);
