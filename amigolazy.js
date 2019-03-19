ffc43404e9a5a842b32a6f8dd7d9ba1d7c6d58ae
Amigo Dheena <AmigoDheena@users.noreply.github.com>
2019-03-19 11:33:22 +0530

    Null
    Default value assigned

--- amigolazy.js
+++ amigolazy.js
@@ -3,6 +3,9 @@ for(let i=0; i<amframe.length;i++){
     let amsrc = amframe[i];
     let amdata = amsrc.getAttribute("data-src");
     let datanew = amsrc.getAttribute("lazy");
+    if(datanew === null){
+        datanew = 1500;
+    }
     setTimeout(function(){
     amframe[i].setAttribute("src",amdata);
     console.info(datanew + "ms Lazyloaded " + amframe[i].src);
